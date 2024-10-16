7451cb94c93dd0be956d5e44ace1f7b818af77d0
GeorgeGogos <george_gogos3@hotmail.com>
2024-10-16 12:38:15 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/GeorgeGogos/goaap
+
+go 1.23.2
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/gorilla/handlers v1.5.2
+	github.com/gorilla/mux v1.8.1
+	github.com/gorilla/websocket v1.5.3
+)
+
+require github.com/felixge/httpsnoop v1.0.4 // indirect
---
+++ internal/app/server/server.go
@@ -0,0 +1,37 @@
+package goapp
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/GeorgeGogos/goaap/internal/pkg/httpsrv"
+	"github.com/GeorgeGogos/goaap/internal/pkg/strgen"
+)
+
+func Start(exitChannel chan os.Signal) error {
+	var (
+		strChan = make(chan string, 100) // String channel with max parallel counter processes.
+		strCli  = strgen.New(strChan)    // String generator.
+		httpSrv = httpsrv.New(strChan)   // HTTP server.
+	)
+
+	// Start String Generator.
+	if err := strCli.Start(); err != nil {
+		return fmt.Errorf("failed to start string generator: %w", err)
+	}
+	defer strCli.Stop()
+
+	// Start HTTP server.
+	if err := httpSrv.Start(); err != nil {
+		return fmt.Errorf("failed to start HTTP server: %w", err)
+	}
+	defer httpSrv.Stop()
+
+	log.Println("GoApp Started")
+	defer log.Println("GoApp Stopped")
+
+	<-exitChannel
+
+	return nil
+}
